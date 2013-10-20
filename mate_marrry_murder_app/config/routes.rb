789c2ca56cd5f196fca7ad633feaa2c47cfb6535
DeProdigy <alex.hint.ah@gmail.com>
2013-10-20 15:13:25 -0400

    setting up omni-auth

--- mate_marrry_murder_app/config/routes.rb
+++ mate_marrry_murder_app/config/routes.rb
@@ -1,4 +1,6 @@
 MateMarrryMurderApp::Application.routes.draw do
+  root to: 'games#index'
+  resources :games
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
