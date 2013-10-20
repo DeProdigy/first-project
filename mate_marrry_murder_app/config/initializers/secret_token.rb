537cd70fc3779f06c0184e0fd978aeeaf6a3712f
DeProdigy <alex.hint.ah@gmail.com>
2013-10-20 13:22:52 -0400

    created the empty app

---
+++ mate_marrry_murder_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MateMarrryMurderApp::Application.config.secret_token = '17f8d1d8e9aa991327fbeeb215d4a4fb4ec5b1d5c1da3ba51352124257cce6439fdb095fe1d1f328b454b24ca67b7d0c1ec5240401687dcc6d897fda148a9588'
