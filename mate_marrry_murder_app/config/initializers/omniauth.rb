0e7f100f17a72b4dc4516f2d8588647616aa3a31
DeProdigy <alex.hint.ah@gmail.com>
2013-10-20 19:11:52 -0400

    setting up fb_graph

--- mate_marrry_murder_app/config/initializers/omniauth.rb
+++ mate_marrry_murder_app/config/initializers/omniauth.rb
@@ -1,5 +1,12 @@
 OmniAuth.config.logger = Rails.logger
 
+# #authentication error problem
+# Omniauth.config do |config|
+#   config.on_failure do
+#     # your handling code invoked in the context of a rack app
+#   end
+# end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY_MMM'], ENV['FACEBOOK_SECRET_MMM']
 end
